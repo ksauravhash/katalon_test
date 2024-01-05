6a54bf111ae27ef1de58a149877a8c2d944443f5
Saurav Kumar <ksauravhash@gmail.com>
2024-01-05 19:32:32 +0530

    Added Test Case

---
+++ Test Suites/CuraTestSuit.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CuraTestSuit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>dea83818-182f-4717-a84a-abb50f022da7</testSuiteGuid>
+   <testCaseLink>
+      <guid>64d20581-36f3-45b4-a0b0-3e2921a60228</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CuraTestCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
